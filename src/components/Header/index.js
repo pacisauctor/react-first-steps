aafd7a18c78be3ab3da3ac588ef8a40b531031ee
Axel Garcia <30581844+pacisauctor@users.noreply.github.com>
2023-07-14 23:20:37 -0600

    feat: init repo

---
+++ src/components/Header/index.js
@@ -0,0 +1,22 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Nombre de la Página</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Doc React</a>
+          </li>
+          <li>
+            <a href="#">Github de Axel</a>
+          </li>
+          <li>
+            <a href="#">Correo</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
